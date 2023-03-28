7309c3533e7b0417901c32ecc5b57ac1bc6420cf
James MacMillan <jamesiain@me.com>
2023-03-28 15:11:44 -0400

    Add "chrisgrieser/nvim-spider" plugin to editor
    This plugin enhances the w e b (and ge) motions to better handle
    CamelCase and unusual (strange) or just-plain-weird punctuation in a
    more humane (and human) way. This is especially useful when traversing
    through source code, which tends to use these types of markup a lot.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,4 +3,42 @@ return {
   { -- open "filename:248,13" at line 248 and column 13
     "wsdjeg/vim-fetch",
   },
+  { -- use w e b motions like a spider (enhanced CamelCase & punctuation traversal)
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        "w",
+        function()
+          require("spider").motion("w")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Next beginning-of-word (Spider)",
+      },
+      {
+        "e",
+        function()
+          require("spider").motion("e")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Next end-of-word (Spider)",
+      },
+      {
+        "b",
+        function()
+          require("spider").motion("b")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Previous beginning-of-word (Spider)",
+      },
+      {
+        "ge",
+        function()
+          require("spider").motion("ge")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Previous end-of-word (Spider)",
+      },
+    },
+    lazy = true,
+  },
 }
