ea3968dc5ac24ff87c146bce91998f2c7af3b9f9
James MacMillan <james.macmillan@allenvanguard.com>
2023-04-13 15:34:24 -0400

    Add "kevinhwang91/nvim-ufo" plugin to coding
    This plugin adds a modern and high-performance set of Ultra Folding
    Operations to NeoVim. This can make it quicker and easier to understand
    code flow by selectively collapsing blocks within the current view.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -13,4 +13,18 @@ return {
       "gisphm/vim-gitignore", -- .gitignore syntax highlighting and snippets
     },
   },
+
+  { -- Ultra Fold Operations (modern, high-performance code folding)
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async", "neovim/nvim-lspconfig" },
+    --stylua: ignore
+    keys = {
+      { "zR", function() require("ufo").openAllFolds() end, desc = "Open All Folds", },
+      { "zM", function() require("ufo").closeAllFolds() end, desc = "Close All Folds", },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("ufo").setup(opts)
+    end,
+  },
 }
