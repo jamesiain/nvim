b7801fe0e80ef084091f3f881da2c261b17a2a13
James MacMillan <jamesiain@me.com>
2023-08-15 12:13:29 -0400

    Add "folke/zen-mode.nvim" plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,19 @@
 return {
 
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      {
+        "<leader>uz",
+        function()
+          require("zen-mode").toggle()
+        end,
+        mode = { "n" },
+        desc = "Toggle Zen-mode",
+      },
+    },
+    opts = {},
+  },
   { -- open "filename:248,13" at line 248 and column 13
     "wsdjeg/vim-fetch",
   },
